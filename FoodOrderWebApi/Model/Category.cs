750379d0357e7fea7321e119c62cf75774b65e98
Levente Máté Smid <levente3000123@gmail.com>
2024-02-11 20:13:03 +0100

    Create Model for db

---
+++ FoodOrderWebApi/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrderWebApi.Model
+{
+    public class Category
+    {
+        [Key]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string PictureName { get; set; } = null!;
+        
+        public virtual ICollection<Product> Products { get; set; } = null!;
+    }
+}
---
+++ FoodOrderWebApi/Model/OpeningHour.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodOrderWebApi.Model
+{
+    public class OpeningHour
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Restaurant")]
+        public int RestaurantId { get; set; }
+        public virtual Restaurant Restaurant { get; set; } = null!;
+
+        public DateTime? Monday { get; set; }
+
+        public DateTime? Tuesday { get; set; }
+
+        public DateTime? Wednesday { get; set; }
+
+        public DateTime? Thursday { get; set; }
+
+        public DateTime? Friday { get; set; }
+
+        public DateTime? Saturday { get; set; }
+
+        public DateTime? Sunday { get; set; }
+    }
+}
