70e403e88fbbbbf4de83ceb6cfa7722224f0b0bf
Levente Máté Smid <levente3000123@gmail.com>
2024-04-19 20:53:30 +0200

    fix: Change the path of RestaurantRepository

---
+++ FoodOrderWebApi/Repositories/Interfaces/IRestaurantRepository.cs
@@ -0,0 +1,14 @@
+﻿using FoodOrderWebApi.Models;
+
+namespace FoodOrderWebApi.Repositories.Interfaces;
+
+public interface IRestaurantRepository : IRepository<Restaurant, int>
+{
+    public ICollection<FoodCategory>? GetRestaurantFoodCategoriesWithProducts(int restaurantId);
+
+    public void CreateRestaurant(Restaurant restaurant);
+
+    public Restaurant? GetByIdAsTracking(int key);
+
+    public void UpdateRestaurant(Restaurant restaurant);
+}
